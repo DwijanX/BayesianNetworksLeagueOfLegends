fae3fcd1dbbedf1c9b69560f824d117a39c85acd
Christian Rivero <christianriveroarnez@gmail.com>
2023-04-19 15:39:46 -0400

    summoner info extracted from the top 200

--- getPUUiDs.py
+++ getPUUiDs.py
@@ -14,11 +14,21 @@ def getPUUIDPerPlayer(region,id):
 with open('top200PlayersEUW.json', 'r') as f:
     top200PlayersEUW = json.load(f)
 
-summonersPUUID = []
+print(top200PlayersEUW["entries"][0]["summonerId"])
 
+summonerInfo ={}
+counter=0
 for player in top200PlayersEUW["entries"]:
-   summonersPUUID.append(getPUUIDPerPlayer(region, player["summonerId"]))
-   time.sleep(10)
+    summonerInfoRetrieved=getPUUIDPerPlayer(region, player["summonerId"])
+    print("got ",summonerInfoRetrieved["name"])
+    summonerInfo[player["summonerId"]]=summonerInfoRetrieved
+    time.sleep(1)
+    if(counter==98 or counter==196):
+        time.sleep(120)
+    counter+=1
 
-with open('summonersPUUID.json', 'w') as f:
-   json.dump(summonersPUUID, f)
+
+json_object = json.dumps(summonerInfo, indent=4)
+
+with open("summonerInfo.json", "w") as outfile:
+    outfile.write(json_object)
