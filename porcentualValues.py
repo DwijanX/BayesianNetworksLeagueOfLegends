9ca526f25f03d25c6d28b36d626d3cb4882652de
Christian Rivero <christianriveroarnez@gmail.com>
2023-04-24 13:34:07 -0400

    calculated some probs

--- porcentualValues.py
+++ porcentualValues.py
@@ -1,78 +1,49 @@
 import pandas as pd
 
 df = pd.read_csv('DataSetLeagueOfLegends1.csv')
-
-#print(df)
+def calculateWinsBalance(df):
+    positions=["Top","Jungle","Mid","ADC","Support",]
+    for position in positions:
+        df[f'TeamBlue{position}winxWr']=df[f'TeamBlue{position}Wins']*df[f'TeamBlue{position}WinRate']-df[f'TeamBlue{position}Losses']*df[f'TeamBlue{position}WinRate']
+        df[f'TeamRed{position}winxWr']=df[f'TeamRed{position}Wins']*df[f'TeamRed{position}WinRate']-df[f'TeamRed{position}Losses']*df[f'TeamRed{position}WinRate']
+calculateWinsBalance(df)
+print(df)
 def getBalanceTwoColumns(column1,column2,df):
     return df[column1]-df[column2]
 
-totalData = 2865
-
-
+totalData = 2866
 damageDataFrame=pd.DataFrame()
 damageDataFrame["win"]=df["win"]
-#golddamageDataFrame["balanceGold"]=getBalanceTwoColumns("TeamBlueTopDamageDealt","TeamRedTopDamageDealt",df)
-#damageDataFrame["balanceGold"]=getBalanceTwoColumns("TeamBlueTopGoldEarned","TeamRedTopGoldEarned",df)
-damageDataFrame["probabilitiesTop"]=getBalanceTwoColumns("TeamBlueTopDamageDealt","TeamRedTopDamageDealt",df)
-damageDataFrame["probabilitiesJg"]=getBalanceTwoColumns("TeamBlueJungleDamageDealt","TeamRedJungleDamageDealt",df)
-damageDataFrame["probabilitiesMid"]=getBalanceTwoColumns("TeamBlueMidDamageDealt","TeamRedMidDamageDealt",df)
-damageDataFrame["probabilitiesADC"]=getBalanceTwoColumns("TeamBlueADCDamageDealt","TeamRedADCDamageDealt",df)
-damageDataFrame["probabilitiesSupp"]=getBalanceTwoColumns("TeamBlueSupportDamageDealt","TeamRedSupportDamageDealt",df)
-print(damageDataFrame)
-probabilitiedDF=pd.DataFrame()
 
-redTopAdvantage=0
-blueTopAdvantage=0
+def sumToPositionSideIfPosOrNeg(balance,positionWinsList:list):
 
-redJgAdvantage=0
-blueJgAdvantage=0
-
-redMidAdvantage=0
-blueMidAdvantage=0
-
-redADCAdvantage=0
-blueADCAdvantage=0
-
-redSuppAdvantage=0
-blueSuppAdvantage=0
-
-for index, row in damageDataFrame.iterrows():
-    if row['probabilitiesTop'] > 0:
-        blueTopAdvantage += 1
+    if balance> 0:
+        positionWinsList[0] += 1
     else:
-        redTopAdvantage += 1
-            
-    if row['probabilitiesJg'] > 0:
-        blueJgAdvantage += 1
-    else:
-        redJgAdvantage += 1
-            
-    if row['probabilitiesMid'] > 0:
-        blueMidAdvantage += 1
-    else:
-        redMidAdvantage += 1
-            
-    if row['probabilitiesADC'] > 0:
-        blueADCAdvantage += 1
-    else:
-        redADCAdvantage += 1
-            
-    if row['probabilitiesSupp'] > 0:
-        blueSuppAdvantage += 1
-    else:
-        redSuppAdvantage += 1
+        positionWinsList[1] += 1
 
-print("Red Top Advantage:", round(redTopAdvantage/totalData, 5))
-print("Blue Top Advantage:", round(blueTopAdvantage/totalData, 5))
+def getProbabilitiesOfStats(stat,srcDf):
+    statDataFrame=pd.DataFrame()
+    positions=["Top","Jungle","Mid","ADC","Support",]
 
-print("Red Jg Advantage:", round(redJgAdvantage/totalData, 5))
-print("Blue Jg Advantage:", round(blueJgAdvantage/totalData, 5))
+    statDataFrame["win"]=srcDf["win"]
+    for position in positions:
+        statDataFrame[f'balance{position}']=getBalanceTwoColumns(f'TeamBlue{position}{stat}',f'TeamRed{position}{stat}',srcDf)
+    Sums={"Top":[0,0],"Jungle":[0,0],"Mid":[0,0],"ADC":[0,0],"Support":[0,0]}
 
-print("Red Mid Advantage:", round(redMidAdvantage/totalData, 5))
-print("Blue Mid Advantage:", round(blueMidAdvantage/totalData, 5))
+    for index, row in statDataFrame.iterrows():
+        sumToPositionSideIfPosOrNeg(row['balanceTop'],Sums["Top"])
+        sumToPositionSideIfPosOrNeg(row['balanceJungle'],Sums["Jungle"])
+        sumToPositionSideIfPosOrNeg(row['balanceMid'],Sums["Mid"])
+        sumToPositionSideIfPosOrNeg(row['balanceADC'],Sums["ADC"])
+        sumToPositionSideIfPosOrNeg(row['balanceSupport'],Sums["Support"])
+    Probs={}
+    for position in positions:
+        Probs[position]=[round(Sums[position][0]/totalData, 5),round(Sums[position][1]/totalData, 5)]
+    return Probs
+#probGold=getProbabilitiesOfStats("GoldEarned",df)
+#probDmg=getProbabilitiesOfStats("DamageDealt",df)
 
-print("Red ADC Advantage:", round(redADCAdvantage/totalData, 5))
-print("Blue ADC Advantage:", round(blueADCAdvantage/totalData, 5))
+probWR=getProbabilitiesOfStats("winxWr",df)
 
-print("Red Supp Advantage:", round(redSuppAdvantage/totalData, 5))
-print("Blue Supp Advantage:", round(blueSuppAdvantage/totalData, 5))
+print(probWR)
